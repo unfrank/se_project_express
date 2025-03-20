767ccd73e66accf9471df92bfd4eef2487a52808
James Unthank <james@unfrank.com>
2025-03-20 12:59:14 -0400

    tidy before submit

--- app.js
+++ app.js
@@ -22,12 +22,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-// Debugging: Log all incoming requests
-app.use((req, res, next) => {
-  console.log(`🟡 ${req.method} ${req.url}`);
-  next();
-});
-
 app.use("/", routes);
 
 app.use((err, req, res, next) => {
