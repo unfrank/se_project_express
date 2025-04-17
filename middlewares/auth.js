d0c3fdf7a5662ce8cddddde43416aad8b582f183
James Unthank <james@unfrank.com>
2025-04-17 13:29:49 -0400

    Ready for Review

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { UNAUTHORIZED } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
-const token = jwt.sign({ _id: user._id }, JWT_SECRET);
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization?.replace("Bearer ", "");
