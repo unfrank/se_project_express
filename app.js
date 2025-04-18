c0c439e57ae823219497290a17784dc8ffca8536
James Unthank <james@unfrank.com>
2025-04-18 11:47:03 -0400

    Apply celebrate, centralized error handling

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-
 const helmet = require("helmet");
 const mongoose = require("mongoose");
-
 const cors = require("cors");
 const { errors } = require("celebrate");
 
 const routes = require("./routes");
-
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const errorHandler = require("./middlewares/error-handler");
 const rateLimiter = require("./middlewares/rate-limiter");
@@ -27,30 +24,24 @@ mongoose.connect(MONGODB_URI).catch(() => process.exit(1));
 
 app.use(helmet());
 
-app.options("*", cors());
+const corsOptions = {
+  origin: "https://unfrank.crabdance.com",
+  credentials: true,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use("/", routes);
-// app.use(cors());
-
-app.use(
-  cors({
-    origin: "https://unfrank.crabdance.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(rateLimiter);
-
 app.use(express.json());
-
 app.use(requestLogger);
 
+app.use("/", routes);
+
 app.use(errorLogger);
-
 app.use(errors());
-
 app.use(errorHandler);
 
 app.listen(PORT);
