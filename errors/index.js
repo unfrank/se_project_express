e6dfadb0a02e94488ab4af3acca5640687629ba9
James Unthank <james@unfrank.com>
2025-04-19 19:07:24 -0400

    braces

--- errors/index.js
+++ errors/index.js
@@ -1,8 +1,8 @@
-const BadRequestError = require("./BadRequestError");
-const ConflictError = require("./ConflictError");
-const ForbiddenError = require("./ForbiddenError");
-const NotFoundError = require("./NotFoundError");
-const UnauthorizedError = require("./UnauthorizedError");
+const { BadRequestError } = require("./BadRequestError");
+const { ConflictError } = require("./ConflictError");
+const { ForbiddenError } = require("./ForbiddenError");
+const { NotFoundError } = require("./NotFoundError");
+const { UnauthorizedError } = require("./UnauthorizedError");
 
 module.exports = {
   BadRequestError,
